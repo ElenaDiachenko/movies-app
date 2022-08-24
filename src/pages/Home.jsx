3594e18d229eb0bbfecc208bbbbac601431a05cd
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-08-24 04:33:50 +0300

    upd Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,24 @@ import { HomeList } from 'components/HomeList/HomeList';
 import { fetchTrendingMovies } from 'services/APP';
 import { Container } from 'components/Container/Container';
 import { Header } from 'components/Header/Header';
+import { Loader } from 'components/Loader/Loader';
 
 export const IMG_PATH = 'https://image.tmdb.org/t/p/w500/';
 
 export const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     try {
+      setLoading(true);
       (async function getMovies() {
         const response = await fetchTrendingMovies();
         setMovies(response);
+        setLoading(false);
       })();
     } catch (error) {
+      setLoading(false);
       console.log(error.message);
     }
   }, []);
@@ -24,10 +29,15 @@ export const Home = () => {
     <>
       <Header />
       <main>
-        <Container>
-          <h1>Trending today</h1>
-          <HomeList movies={movies} />
-        </Container>
+        {loading && <Loader />}
+        {movies ? (
+          <Container>
+            <h1>Trending today</h1>
+            <HomeList movies={movies} />
+          </Container>
+        ) : (
+          <p>Sorry </p>
+        )}
       </main>
     </>
   );
