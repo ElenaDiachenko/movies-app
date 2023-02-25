45348dd93dfb8ad0d7b68fe6f95f2fa762d9bb0b
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2023-02-25 03:37:50 +0200

    save liked mpvie

--- src/components/Slider/SliderCard/SliderCard.tsx
+++ src/components/Slider/SliderCard/SliderCard.tsx
@@ -1,8 +1,12 @@
 import { FC, useState, MouseEvent } from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { useLocation, Link } from 'react-router-dom';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { shallow } from 'zustand/shallow';
 import { motion } from 'framer-motion';
 
+import { db } from 'config/firebase';
+import { useStore } from 'stores/store';
 import { IMG_PATH } from 'pages/Home';
 import {
   StyledCard,
@@ -22,11 +26,36 @@ interface SliderCardProps {
 export const SliderCard: FC<SliderCardProps> = ({ movie }) => {
   const { id, title, poster_path } = movie;
   const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
   const location = useLocation();
+  const { user } = useStore(
+    state => ({
+      user: state.authUser,
+    }),
+    shallow
+  );
 
-  const toggleLike = (e: MouseEvent<HTMLDivElement>) => {
+  const movieID = doc(db, 'users', `${user?.email}`);
+
+  const saveMovie = async (e: MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
-    setLike(like => !like);
+    if (user?.email) {
+      try {
+        setLike(like => !like);
+        setSaved(true);
+        await updateDoc(movieID, {
+          savedMovies: arrayUnion({
+            id: movie.id,
+            title: movie.title,
+            img: movie.poster_path,
+          }),
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert('Please log in to save a movie');
+    }
   };
 
   return (
@@ -38,7 +67,7 @@ export const SliderCard: FC<SliderCardProps> = ({ movie }) => {
               <MoviePoster src={IMG_PATH + poster_path} alt={title} />
               <Content>
                 <Title>{title}</Title>
-                <Like onClick={toggleLike}>
+                <Like onClick={saveMovie}>
                   {like ? (
                     <FaHeart size={25} color={'red'} />
                   ) : (
