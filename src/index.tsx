4945c6adeedcd7964d61b11f0e253bfb1f04b064
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2023-03-06 22:41:22 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
